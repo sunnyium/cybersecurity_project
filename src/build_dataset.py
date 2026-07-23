7cb77871464e891635346b83bd16369345c84805
Sunny Li <lixinyangsunny@gmail.com>
2026-07-23 16:52:55 -0600

    Basic Data Cleaning

---
+++ src/build_dataset.py
@@ -0,0 +1,18 @@
+
+import os
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
+TARGET = "Label"
+
+# Files in chronological order
+FILE_ORDER = [
+    "Monday-WorkingHours.pcap_ISCX.csv",
+    "Tuesday-WorkingHours.pcap_ISCX.csv",
+    "Wednesday-workingHours.pcap_ISCX.csv",
+    "Thursday-WorkingHours-Morning-WebAttacks.pcap_ISCX.csv",
+    "Thursday-WorkingHours-Afternoon-Infilteration.pcap_ISCX.csv",
+    "Friday-WorkingHours-Morning.pcap_ISCX.csv",
+    "Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv",
+    "Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv",
+]
+
